226f0cd656e4852c30904ad54e53a7d2f6541709
Sarfraz212000 <sarfrazprogrammer@gmail.com>
2026-05-20 13:39:04 +0530

    for deploy sb app in openshift

---
+++ src/main/java/in/saffu/rest/OpenshiftController.java
@@ -0,0 +1,14 @@
+package in.saffu.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenshiftController {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from OpenShift!";
+	}
+
+}
